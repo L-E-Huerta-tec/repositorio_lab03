411aac4e3b18fe7ad71b14b5c52f1264f6618967
Luis_Huerta <luis.huerta@tecsup.edu.pe>
2023-09-27 22:55:29 -0500

    laboratorio terminado

---
+++ ejercicio6.js
@@ -0,0 +1,10 @@
+let nombreCompleto = "Juan Perez";
+let nombreCompleto2 = "Carlos Lara";
+console.log( nombreCompleto);
+console.log(nombreCompleto2);
+
+let a1nombreCompleto;
+let _nomreCompleto;
+let $nombreCompleto;
+
+let ruptura = 10;
